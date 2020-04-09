8bbc002a5087b49635fe313fd448370135026103
JacksonPaulHeck <jacksonheck1@gmail.com>
2020-04-09 01:27:03 -0500

    Final Push

--- test.cpp
+++ test.cpp
@@ -5,7 +5,36 @@
 #include "catch.hpp"
 #include "JPStack.h"
 
-TEST_CASE("JPLinkedList Testing");
+TEST_CASE("JPLinkedList Testing"){
+    SECTION("JPLinkedList Test"){
+        JPLinkedList<int> jpLinkedList;
+        jpLinkedList.append(0);
+        jpLinkedList.append(1);
+        jpLinkedList.append(2);
+        jpLinkedList.append(3);
+        jpLinkedList.append(4);
+        jpLinkedList.append(5);
+
+        REQUIRE(jpLinkedList.contains(0));
+        REQUIRE(jpLinkedList.contains(1));
+        REQUIRE(jpLinkedList.contains(2));
+        REQUIRE(jpLinkedList.contains(3));
+        REQUIRE(jpLinkedList.contains(4));
+        REQUIRE(jpLinkedList.contains(5));
+
+        REQUIRE(jpLinkedList.length() == 6);
+
+        REQUIRE(!jpLinkedList.isEmpty());
+
+        jpLinkedList.remove();
+
+        REQUIRE(jpLinkedList.length() == 5);
+
+        jpLinkedList.insert(-1);
+
+        REQUIRE(jpLinkedList.length() == 6);
+    }
+}
 
 TEST_CASE("JPStack Testing") {
     JPString jpString("JPString");
